eca4ce6e31e163499b4532d2399235a05caa07ba
SandilaAnwar579 <ba3522165@gmail.com>
2025-07-14 14:42:53 -0700

    Add files via upload

---
+++ exporter.js
@@ -0,0 +1,14 @@
+// exporter.js
+
+export const studentName = "Fatima";
+
+export const numbers = [10, 20, 30];
+
+export const studentInfo = {
+  name: "Ahmed",
+  marks: 85
+};
+
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
